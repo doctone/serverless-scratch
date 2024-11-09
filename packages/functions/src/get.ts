ca4835c65e757f9ba47e4ab0d4c4213a6a72a7ee
Sam James <sam.james@novata.com>
2024-11-09 23:25:14 +0000

    add aws cognito, secure api routes

--- packages/functions/src/get.ts
+++ packages/functions/src/get.ts
@@ -9,7 +9,6 @@ export const main = Util.handler(async (event) => {
   const params = {
     TableName: Resource.Notes.name,
     Key: {
-      userId: "123",
       noteId: event?.pathParameters?.id,
     },
   };
